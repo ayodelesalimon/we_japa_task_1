61b804963b82f75a61a6eb9cffb5768888a51888
SALIMONU AYODELE KABIR <39867606+ayodelesalimon@users.noreply.github.com>
2020-07-28 04:07:51 +0100

    Add files via upload

---
+++ src/components/MyName.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="name">
+
+       <h1>{{name}}</h1> 
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+    name: String,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
